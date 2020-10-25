05df9b32e499c9edb4ce55f4820c14c56e4ba5a6
Michal Biedzki <mbiedzki@me.com>
2020-10-25 17:25:39 +0100

    init

---
+++ src/app/map/map/map.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import VectorLayer from 'ol/layer/Vector';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+import TileLayer from 'ol/layer/Tile';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+  map: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.map = new Map({
+      target: 'users_map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: olProj.fromLonLat([21.012229, 52.229676]),
+        zoom: 8
+      })
+    });
+  }
+
+}
